560138fffb10acbe8c92f2f47bc3300c6a6219b3
hnrchrdl <hinrich@posteo.de>
2018-03-04 08:35:21 +0100

    fix: post details update

--- src/components/post-details/PostDetails.js
+++ src/components/post-details/PostDetails.js
@@ -5,31 +5,33 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Post } from '../../components';
 import { connect } from 'react-redux';
-import { fetchPost } from '../../actions';
+import { fetchPosts } from '../../actions';
 
 class PostDetails extends Component {
-
     componentDidMount() {
-        if(this.props.match.params && this.props.match.params.postid)
-        this.props.fetchPost(this.props.match.params.postid)
+        // fetch a fresh list of all posts
+        this.props.fetchPosts();
     }
 
     render() {
-
+        // get the post from the props
         const { post } = this.props;
         return (
             <div className="post-details">
-                { post && <Post post={post} showDetails={true}/> }
+                {post && <Post post={post} showDetails={true} />}
             </div>
         );
     }
 }
 const mapStateToProps = (state, props) => ({
-    post: state.post.details
+    // from the list of posts, find the one matching the id from url params
+    post: props.match.params
+        ? state.post.posts.find(post => post.id === props.match.params.postid)
+        : null
 });
 
 const mapDispatchToProps = dispatch => ({
-    fetchPost: fetchPost(dispatch)
+    fetchPosts: fetchPosts(dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PostDetails);
