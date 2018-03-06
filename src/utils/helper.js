69c967d8205f045d7294733c5230c717e20a058f
hnrchrdl <hinrich@posteo.de>
2018-03-06 21:44:39 +0100

    chore(helper.js): correctly attributes obtained code

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,6 +1,15 @@
 
 export const prettyDate = time => {
-    // https://stackoverflow.com/questions/7641791/javascript-library-for-human-friendly-relative-date-formatting
+
+    /* 
+     * JavaScript Pretty Date
+     * Copyright (c) 2011 John Resig (ejohn.org)
+     * Licensed under the MIT and GPL licenses.
+     * 
+     * Code found here
+     * https://stackoverflow.com/a/7641822/2024590
+    */
+
     var date = new Date(time),
         diff = (((new Date()).getTime() - date.getTime()) / 1000),
         day_diff = Math.floor(diff / 86400);
@@ -35,7 +44,14 @@ export const prettyDate = time => {
 }
 
 export const sortBy = (field, reverse, primer = x => (x)) => {
-    // https://stackoverflow.com/questions/979256/sorting-an-array-of-javascript-objects
+
+    /* 
+     * By Triptych
+     * 
+     * Found here
+     * https://stackoverflow.com/a/979325/2024590
+    */
+
     var key = primer ?
         function (x) { return primer(x[field]) } :
         function (x) { return x[field] };
@@ -48,7 +64,15 @@ export const sortBy = (field, reverse, primer = x => (x)) => {
 }
 
 export const generateUUID = () => {
-    // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+
+    /* 
+     * From Stackoverflow Community Wiki
+     * Originally by broofa
+     * 
+     * Code found here
+     * https://stackoverflow.com/a/2117523/2024590
+    */
+
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
         return v.toString(16);
